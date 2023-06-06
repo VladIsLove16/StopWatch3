4ce8b5043d2c89a311a8126d72a98938f56df908
KseniaSomova <countistars@gmail.com>
2023-06-06 16:24:01 +0300

    4.4.4

--- StopWatch3/MainWindow.xaml.cs
+++ StopWatch3/MainWindow.xaml.cs
@@ -302,6 +302,11 @@ namespace StopwatchApp
             Console.WriteLine(Tracker.GetTasksByDate((DateTime)calendar.SelectedDate).Count);
             TaskList.Clear();
         }
+
+        private void nameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 
 }
--- StopWatch3/obj/Debug/StopWatch3_Content.g.i.cs
+++ StopWatch3/obj/Debug/StopWatch3_Content.g.i.cs
@@ -8,14 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/angle-right.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/calendar.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/histogram.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/pause.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/pbm-pause.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/play.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/play2.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/pmb-play.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/time.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon-int/trash.png")]
 
 
